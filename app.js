893c7981bd2131d54f0cf16f0318a192e37c2733
Igor123fight <i.borshevski@yandex.ru>
2023-12-05 14:39:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", async (req, res) => {
       console.log(json);
       const dealUpdate = await fetch(`${b24Url}crm.timeline.comment.add?fields[ENTITY_ID]=${idDeal}&fields[ENTITY_TYPE]=deal&fields[COMMENT]=Создался документ Реализации товаров и услуг`)
     } else {
-      alert("Ошибка HTTP: " + getterField.status);
+      console.log("Ошибка HTTP: " + getterField.status);
     }
     res.type('html').send(html) 
   }
